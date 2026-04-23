0907e726d2d1f0c742533028104057518f813121
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-04-23 21:55:17 +0530

    Update

---
+++ GeneralInterviewQuestion/TwoSum.java
@@ -0,0 +1,16 @@
+public class TwoSum {
+    public static void main(String[] args) {
+        
+        int arr[] = {1,2,3,4,5,6};
+        int target = 11;
+
+        for(int i=0;i<arr.length;i++){
+            for(int j =i+1;j<arr.length;j++){
+                if(arr[i] + arr[j] == target){
+                    System.out.println(i + ", "+j);
+                }
+            }
+        }
+        
+    }
+}
