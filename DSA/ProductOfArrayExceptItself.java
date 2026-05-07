4cf99bc2c7b5cce74fb944edee934480e34e1495
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-05-07 15:28:01 +0530

    DSA

---
+++ DSA/ProductOfArrayExceptItself.java
@@ -0,0 +1,37 @@
+public class ProductOfArrayExceptItself {
+
+    public static int []product(int [] arr){
+
+        int n=arr.length;
+        int left[] = new int[n];
+        int right[] = new int[n];
+        int result[] = new int[n];
+
+        left[0] = 1;
+        for(int i=1;i<n;i++){
+            left[i] = left[i-1] * arr[i-1];
+        }
+
+        right[n-1] =1;
+        for(int i=n-2;i>=0;i--){
+            right[i] = right[i+1] * arr[i+1];
+        }
+
+        for(int i=0;i<n;i++){
+            result[i] = left[i] * right[i];
+        }
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        
+        int arr[] = {1,2,3,4};
+
+        int result[] = product(arr);
+        for(int n: result){
+            System.out.print(n+" ");
+        }
+
+
+    }
+}
