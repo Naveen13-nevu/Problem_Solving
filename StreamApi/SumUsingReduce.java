456a9051be93884ef97d34f7b63a9e46366ef144
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-05-10 12:58:57 +0530

    DSA

--- StreamApi/SumUsingReduce.java
+++ StreamApi/SumUsingReduce.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class SumUsingReduce {
     
-
     public static void main(String[] args) {
         
         List<Integer> list = Arrays.asList(1,2,3,4,5,6);
@@ -10,7 +9,6 @@ public class SumUsingReduce {
         int sum = list.stream()
         .reduce(0, Integer::sum);
         System.out.println(sum);
-
 
     }
 }
