0509dff6075f40634818de8fc2dd7ed8289beb04
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-05-30 17:55:57 +0530

    update

--- StreamApi/FindMax.java
+++ StreamApi/FindMax.java
@@ -6,11 +6,10 @@ public class FindMax {
     
     public static void main(String[] args) {
         
-
         List<Integer> list = Arrays.asList(1,2,3,4,5,6);
 
         Optional<Integer> max = list.stream()
-        .max(Integer::compareTo);
+        .max(Integer::compareTo);       
 
         System.out.println(max.orElse(0));
 
