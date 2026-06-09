7673c86dd2e4fd773d73fb6bf016fe66b981f771
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-06-09 11:21:46 +0530

    Update

--- TVM_Test/Alpha.java
+++ TVM_Test/Alpha.java
@@ -4,8 +4,24 @@ public class Alpha {
         
         String str = "How was the program today";
         String[] arr = str.split(" ");
-       
-        
+        String res ="";
+
+        for(int i=0;i<arr.length;i++){
+            String word = arr[i];
+            char ch[] = word.toCharArray();
+            for(int j=0;j<ch.length-1;j++){
+                for(int k=j+1;k<ch.length;k++){
+                    if(ch[j] > ch[k]){
+                        char temp = ch[j];
+                        ch[j] = ch[k];
+                        ch[k] = temp;
+                    }
+                }
+            }
+            String s = new String(ch);
+            res = res + s+" ";
+        }
+        System.out.println(res);
         
     }
 
