429447a09796aeaf79042f62a5bed565c820928b
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-06-25 10:55:36 +0530

    String count

---
+++ Practice/StringCount.java
@@ -0,0 +1,13 @@
+
+
+class StringCount {
+
+    public static void main(String[] args) {
+        
+        String str1 =   "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJOYXZlZW4iLCJpYXQiOjE3ODIzNjQ0ODIsImV4cCI6MTc4MjM2ODA4Mn0.PXeHaYOJGhWTBbKvX7kb0P5MEeiiQOyKVzDQjVDxdQBQ4q353H4CswhjkByBytgL";
+    
+        System.out.println(str1.length());
+
+    }
+    
+}
