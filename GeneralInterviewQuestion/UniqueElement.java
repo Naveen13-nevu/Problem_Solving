f887c2a6dfa86b62ac67208cd5a6b710374955c2
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-07-20 14:11:56 +0530

    update

---
+++ GeneralInterviewQuestion/UniqueElement.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class UniqueElement {
+    
+    public static void main(String[] args) {
+        
+        int arr[] = {1,2,2,3,3,4,4,5};
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int num:arr){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+        }
+        for(int key : map.keySet()){
+            if(map.get(key) == 1){
+                System.out.print(key + " ");
+            }
+        }
+
+
+    }
+}
