a7f6cf16bb098aa4b3b6f3096ad8a7909a34c216
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-08-12 13:00:22 +0530

    solved water problem

---
+++ DSA/MostWater.java
@@ -0,0 +1,39 @@
+public class MostWater {
+
+    public static  int maxArea(int [] height){
+
+        int left = 0;
+        int right = height.length-1;
+
+        int maxWater = 0;
+
+        while (left < right){
+            int width = right - left;
+
+            int waterHeight = Math.min(height[left], height[right]);
+
+            int currentWater = width * waterHeight;
+
+            maxWater = Math.max(maxWater, currentWater);
+
+            if(height[left] < height[right]){
+                left++;
+            }
+            else{
+                right --;
+            }
+
+        }
+        return maxWater;
+
+    }
+    
+    public static void main(String[] args) {
+
+        int arr[] = {1,8,6,2,5,4,8,3,7};
+
+        System.out.println(maxArea(arr)); 
+
+    }
+
+}
