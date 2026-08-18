18c8af6c5fba58aa3d427b5a4706bdfe2e245559
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-08-18 16:03:40 +0530

    update

--- DSA/TrappingWaterOptimized.java
+++ DSA/TrappingWaterOptimized.java
@@ -40,12 +40,10 @@ public class TrappingWaterOptimized {
     }
 
     public static void main(String[] args) {
-        
 
         int arr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
 
         System.out.println(trap(arr));
-
 
     }
 }
