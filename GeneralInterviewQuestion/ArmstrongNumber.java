8e161aba204031071241b0093de2d2a692ddf900
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-08-20 22:41:42 +0530

    update

--- GeneralInterviewQuestion/ArmstrongNumber.java
+++ GeneralInterviewQuestion/ArmstrongNumber.java
@@ -1,22 +1,22 @@
 
+
 public class ArmstrongNumber {
     public static void main(String[] args) {
-        
 
         int n = 153;
-        int orginal = n;
-        int sum =0;
+        int original = n;
+        int numberOfDigits = String.valueOf(n).length();
 
-        while(n>0){
-            int d = n%10;
-            sum = sum + d*d*d;
-            n = n/10;
-        }
-        if(sum == orginal){
-            System.out.println(orginal+" this is armstrong number");
+        int sum = String.valueOf(n).chars()
+                .map(c -> c - '0')
+                .map(digit -> (int) Math.pow(digit, numberOfDigits))
+                .sum();
+
+        if(sum == original){
+            System.out.println(original+" this is armstrong number");
         }
         else{
-             System.out.println(orginal+" this is not a armstrong number");
+             System.out.println(original+" this is not a armstrong number");
         }
     }
 }
